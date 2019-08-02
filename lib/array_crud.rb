c9b0dddfca35f74738625fa4e650342a57338a52
Adam Finch <atyreefinch@gmail.com>
2019-08-02 21:19:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,12 +2,12 @@ def create_an_empty_array
   []
 end
 
-def create_an_array(arr,thing)
-  arr<<thing
+def create_an_array
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr<<thing
 end
 
 def add_element_to_start_of_array(array, element)
